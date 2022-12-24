089fd19583d7713e5fea05e844e2ab542c8a8806
CristinaT21 <93125408+CristinaT21@users.noreply.github.com>
2022-12-24 05:33:17 +0200

    Add files via upload

---
+++ flaskProject/app.py
@@ -0,0 +1,147 @@
+import os
+from flask import Flask, render_template, request, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+from algorithm import *
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database_2.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+df = pd.read_csv('short_recipes.csv')
+liked = []
+disliked = []
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(100), nullable=False)
+    lastname = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(80), nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.firstname}>'
+
+
+class Recipe(db.Model):
+    Calories = db.Column(db.Float, nullable=True)
+    CarbohydrateContent = db.Column(db.Float, nullable=True)
+    CholesterolContent = db.Column(db.Float, nullable=True)
+    FatContent = db.Column(db.Float, nullable=True)
+    FiberContent = db.Column(db.Float, nullable=True)
+    Images = db.Column(db.String(100), nullable=True)
+    ProteinContent = db.Column(db.String(100), nullable=True)
+    RecipeInstructions = db.Column(db.String(500), nullable=True)
+    RecipeServings = db.Column(db.Integer, nullable=True)
+    SaturatedFatContent = db.Column(db.Float, nullable=True)
+    SodiumContent = db.Column(db.Float, nullable=True)
+    SugarContent = db.Column(db.Float, nullable=True)
+    TotalTime = db.Column(db.String(10), nullable=True)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    ingredients = db.Column(db.String(256), nullable=False)
+    # user_id = db.Column(db.Integer)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
+@app.route('/register', methods=["POST"])
+def hello_world():  # put application's code here
+    firstname = request.json["firstname"]
+    lastname = request.json["lastname"]
+    email = request.json["email"]
+    password = request.json["password"]
+
+    user = User(firstname=firstname, lastname=lastname,
+                email=email, password=password)
+    db.session.add(user)
+    db.session.commit()
+
+    last_id = User.query.order_by(User.id.desc()).first().id
+
+    return {
+        "index" : last_id,
+        "message" : "Success"
+    }, 200
+
+
+
+@app.route('/profile/<int:index>/', methods=["GET"])
+def profile(index):
+    if request.method == "GET":
+
+        user = User.query.filter_by(id=index).first()
+
+        return {
+            "firstname" : user.firstname,
+            "lastname" : user.lastname,
+            "email" : user.email
+        }
+
+@app.route('/recommendation', methods=["POST"])
+def get_recommendation():
+    if request.method == "POST":
+        Calories = request.json["Calories"]
+        CarbohydrateContent = request.json["CarbohydrateContent"]
+        CholesterolContent = request.json["CholesterolContent"]
+        FatContent = request.json["FatContent"]
+        FiberContent = request.json["FiberContent"]
+        Images = request.json["Images"]
+        ProteinContent = request.json["ProteinContent"]
+        RecipeInstructions = request.json["RecipeInstructions"]
+        RecipeServings = request.json["RecipeServings"]
+        SaturatedFatContent = request.json["SaturatedFatContent"]
+        SodiumContent = request.json["SodiumContent"]
+        SugarContent = request.json["SugarContent"]
+        TotalTime = request.json["TotalTime"]
+        id = request.json["RecipeId"]
+        name = request.json["Name"]
+        ingredients = request.json["RecipeIngredientParts"]
+
+
+        recipe = Recipe(Calories=Calories, CarbohydrateContent=CarbohydrateContent,
+                    CholesterolContent=CholesterolContent, FatContent=FatContent, FiberContent=FiberContent,
+                    Images=Images, ProteinContent=ProteinContent, RecipeInstructions=RecipeInstructions,
+                    RecipeServings=RecipeServings, SaturatedFatContent=SaturatedFatContent,
+                    SodiumContent=SodiumContent, SugarContent=SugarContent,
+                    TotalTime=TotalTime, id=id, name=name, ingredients=ingredients)
+        db.session.add(recipe)
+        db.session.commit()
+
+        return {
+                   "id": id,
+                   "message": "Success"
+               }, 200
+
+@app.route('/recommendation/<int:index>/', methods=["GET"])
+def recommendation(index):
+    if request.method == "GET":
+        return similar_dishes_json(df, similar_dishes(index, df, 'transformer.pkl'))
+
+
+
+@app.route('/signin', methods=["GET"])
+def signin():
+    email = request.json["email"]
+    password = request.json["password"]
+
+    user = User.query.filter_by(email=email).first()
+    if user is None:
+        return {
+            "message" : "No such user!"
+        }, 404
+    elif user.password != password:
+        return {
+            "message" : "Wrong password!"
+        }, 400
+    else:
+        return {
+            "message" : "Sing in!"
+        }, 200
+
+
+if __name__ == '__main__':
+    app.run()
